ebb0194f19c8a3d82ff3d6f82b0a1121d5d97478
chugaev <achugaev93@gmail.com>
2017-08-13 11:38:12 +0300

    New @Getter decorator that defines property descriptor with getter that returns instance of specified class using DI container.

---
+++ Source/DI/Decorators/Getter.ts
@@ -0,0 +1,13 @@
+import {Constructor} from '../../types';
+import {Container} from '../Container/Container';
+
+
+export function Getter<T>(type: Constructor<T>): PropertyDecorator {
+    return function (target: object, property: string | symbol): void {
+        Object.defineProperty(target, property, {
+            get(): T {
+                return Container.get(type);
+            }
+        });
+    };
+}
