f1f52e9e3caf9caf15f46083f36137d3162a4205
chugaev <achugaev93@gmail.com>
2017-10-06 21:25:27 +0300

    Enhanced @Profile decorator functionality.

--- Source/Language/Decorators/Profile.ts
+++ Source/Language/Decorators/Profile.ts
@@ -2,38 +2,55 @@ import {DecoratorTarget} from '../Support/Decorators/DecoratorTarget';
 import {Target} from './Target';
 
 
-export function Profile(): MethodDecorator {
+export interface IProfilingConfiguration {
+    printArguments?: boolean;
+    printReturnValue?: boolean;
+    printStack?: boolean;
+}
+
+
+export function Profile(configuration: IProfilingConfiguration = {}): MethodDecorator {
     return (function (
-        prototype: object,
+        prototype: object | Function,
         methodName: string,
         descriptor: TypedPropertyDescriptor<Function>
     ): TypedPropertyDescriptor<Function> {
         Target(DecoratorTarget.Method)(...arguments);
 
+        const {printArguments, printReturnValue, printStack} = configuration;
         const method: Function = descriptor.value as Function;
+        const className: string = typeof prototype === 'function' ? prototype.name : prototype.constructor.name;
 
         descriptor.value = function () {
-            // let error = new Error();
             let startTime: number = Date.now();
 
             /* tslint:disable:no-console */
-            // console.log(`\nProfile method: ${prototype.constructor.name}.${methodName}:`);
-            // console.log(`  Arguments:`, arguments);
-
             let returnValue: any = method.apply(this, arguments);
             let endTime: number = Date.now();
 
-            // console.log(`  Returns:`, returnValue);
-            console.log(`%s :: Elapsed time: %sms`, methodName, (endTime - startTime));
-            // console.log(`  Trace:`);
+            console.log(`%s.%s() - Elapsed time: %sms`, className, methodName, (endTime - startTime));
 
-            // if (error.stack != null) {
-            //     error.stack.split('\n').slice(1).forEach((line) => {
-            //         console.log(`    ` + line.trim());
-            //     });
-            // }
-            //
-            // console.log(``);
+            if (printArguments) {
+                console.log(`  Arguments:`, arguments);
+            }
+
+            if (printReturnValue) {
+                console.log(`  Returns:`, returnValue);
+            }
+
+            if (printStack) {
+                console.log(`  Trace:`);
+
+                const error = new Error();
+
+                if (error.stack != null) {
+                    error.stack.split('\n').slice(1).forEach((line) => {
+                        console.log(`    ` + line.trim());
+                    });
+                }
+            }
+
+            console.log(``);
             /* tslint:enable:no-console */
 
             return returnValue;
