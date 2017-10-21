8040b97adb99db64cf2b51a403663fc3e000ec5b
chugaev <achugaev93@gmail.com>
2017-10-21 17:37:05 +0300

    Improved base implementation of TextParser.

--- Source/Text/Parsing/TextParser.ts
+++ Source/Text/Parsing/TextParser.ts
@@ -6,7 +6,9 @@ export abstract class TextParser<TState extends TextParserState, TResult> {
     public parse(sourceString: string): TResult {
         const state: TState = this.getInitialState(sourceString);
 
-        this.onBeforeParsing(state);
+        if (this.onBeforeParsing) {
+            this.onBeforeParsing(state);
+        }
 
         for (let index = 0; index < sourceString.length; index++) {
             state.index = index;
@@ -15,15 +17,17 @@ export abstract class TextParser<TState extends TextParserState, TResult> {
             this.parseNext(state);
         }
 
-        this.onAfterParsing(state);
+        if (this.onAfterParsing) {
+            this.onAfterParsing(state);
+        }
 
         return this.getResult(state);
     }
 
 
     protected abstract getInitialState(sourceString: string): TState;
-    protected abstract onBeforeParsing(state: TState): void;
+    protected abstract onBeforeParsing?(state: TState): void;
     protected abstract parseNext(state: TState): void;
-    protected abstract onAfterParsing(state: TState): void;
+    protected abstract onAfterParsing?(state: TState): void;
     protected abstract getResult(state: TState): TResult;
 }
