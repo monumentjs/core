1b42817f1b3aba845666b03f2bbe61c383a60086
achugaev <achugaev93@gmail.com>
2019-09-05 10:56:29 +0300

    docs: generate docs for "latest" version + actual version so docs for older versions will be available

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { task, src, series, parallel } = require('gulp');
 const typedoc = require('gulp-typedoc');
 const clean = require('gulp-clean');
+const lernaConfig = require('./lerna');
 
 const OUT_DIR_ROOT = 'package';
 const PACKAGES = [
@@ -47,23 +48,42 @@ task(
     'docs:clean',
     parallel.apply(
       null,
-      PACKAGES.map(pkg => {
-        return () => {
-          return src([`packages/${pkg.name}/index.ts`]).pipe(
-            typedoc({
-              name: `@monument/${pkg.name}`,
-              mode: 'file',
-              exclude: pkg.deps.length > 0 ? `**/packages/+(${pkg.deps.join('|')})/**` : undefined,
-              out: `${OUT_DIR_ROOT}/${pkg.name}/`,
-              readme: `packages/${pkg.name}/README.md`,
-              excludePrivate: true,
-              excludeNotExported: true,
-              gaID: 'UA-129068725-1',
-              tsconfig: './tsconfig.json'
-            })
-          );
-        };
-      })
+      [
+        ...PACKAGES.map(pkg => {
+          return () => {
+            return src([`packages/${pkg.name}/index.ts`]).pipe(
+              typedoc({
+                name: `@monument/${pkg.name}`,
+                mode: 'file',
+                exclude: pkg.deps.length > 0 ? `**/packages/+(${pkg.deps.join('|')})/**` : undefined,
+                out: `${OUT_DIR_ROOT}/${pkg.name}/latest`,
+                readme: `packages/${pkg.name}/README.md`,
+                excludePrivate: true,
+                excludeNotExported: true,
+                gaID: 'UA-129068725-1',
+                tsconfig: './tsconfig.json'
+              })
+            );
+          };
+        }),
+        ...PACKAGES.map(pkg => {
+          return () => {
+            return src([`packages/${pkg.name}/index.ts`]).pipe(
+              typedoc({
+                name: `@monument/${pkg.name}`,
+                mode: 'file',
+                exclude: pkg.deps.length > 0 ? `**/packages/+(${pkg.deps.join('|')})/**` : undefined,
+                out: `${OUT_DIR_ROOT}/${pkg.name}/${lernaConfig.version}`,
+                readme: `packages/${pkg.name}/README.md`,
+                excludePrivate: true,
+                excludeNotExported: true,
+                gaID: 'UA-129068725-1',
+                tsconfig: './tsconfig.json'
+              })
+            );
+          };
+        })
+      ]
     )
   )
 );
